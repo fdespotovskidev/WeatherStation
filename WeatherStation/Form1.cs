584c06ac187fdd5ffe6c1e7ed34b24011e45ce9d
Aleksandar Despotovski <despotovski01@gmail.com>
2017-05-23 12:25:34 +0200

    Fix weather units

--- WeatherStation/Form1.cs
+++ WeatherStation/Form1.cs
@@ -95,7 +95,7 @@ namespace WeatherStation
             lblHumidity.Text = CurrentWeather.Measurement.Humidity.ToString() + "%";
             lblPressure.Text = string.Format("{0} {1}", CurrentWeather.Measurement.Pressure, CurrentWeather.Measurement.PressureUnit);
             lblWindName.Text = CurrentWeather.Measurement.WindName;
-            lblWindSpeed.Text = string.Format("{0:0.00} km/h {1}", CurrentWeather.Measurement.WindSpeed, CurrentWeather.Measurement.WindDirection);
+            lblWindSpeed.Text = string.Format("{0:0.00} {1} {2}", CurrentWeather.Measurement.WindSpeed, CurrentWeather.Units.ToLower() == "metric" ? "m/s" : "mph", CurrentWeather.Measurement.WindDirection);
             lblClouds.Text = string.Format("{0}, {1}%", CurrentWeather.Measurement.CloudsName, CurrentWeather.Measurement.CloudsValue);
             lblLastUpdated.Text = string.Format("{0}, {1}", CurrentWeather.Measurement.LastUpdate.ToShortDateString(), CurrentWeather.Measurement.LastUpdate.ToLocalTime().ToShortTimeString());
             pnlFiveDayWeather.Controls.Clear();
