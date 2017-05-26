230f950315e38d28c6c492516fc9b78061237b41
Aleksandar Despotovski <despotovski01@gmail.com>
2017-05-26 23:59:11 +0200

    Make AboutForm not resizable

--- WeatherStation/AboutForm.Designer.cs
+++ WeatherStation/AboutForm.Designer.cs
@@ -161,6 +161,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnClose);
             this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "AboutForm";
             this.Text = "About WeatherStation";
